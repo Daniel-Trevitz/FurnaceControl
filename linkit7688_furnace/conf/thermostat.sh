abbd6c0dde731b3e5280cb3c6b42e4e727ef8539
Daniel Trevitz <Daniel-Trevitz@users.noreply.github.com>
2019-02-19 20:46:01 -0500

    Make GUI work on the phone, paying little heed to the desktop's appearence

--- linkit7688_furnace/conf/thermostat.sh
+++ linkit7688_furnace/conf/thermostat.sh
@@ -4,6 +4,11 @@ START=99
 STOP=15
 
 start() {
+        echo "Starting local web server"
         uhttpd -h /root/ -p 8080
+        echo "Starting remote web server"
+        uhttpd -h /root/ -s 8081 -D -C /root/furnace.crt -K /root/furnace.key -c /root/httpd.conf
+        echo "Starting controller"
         sh /root/thermostat.sh &
+        echo "Done"
 }
